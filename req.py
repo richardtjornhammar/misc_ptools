00346c0ad2ef925369334b59deffe2e891aad68f
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2018-09-24 15:48:09 +0200

    Create req.py

---
+++ req.py
@@ -0,0 +1,12 @@
+import requests
+
+def query_hmdb(metabolite):
+    query_what = ["http://www.hmdb.ca/unearth/q?query=\'","\';searcher=metabolites"]
+    r = requests.get( metabolite.join(query_what) )
+    return([s for s in str(r.content).split('/') if 'HMDB' in s ])
+#
+metabolite = 'Phenylalanine (13C9) [M-H]-'
+print ( query_hmdb(metabolite) )
+#
+metabolite = 'Phenylalanine (13C9) [M+H]+'
+print ( query_hmdb(metabolite) )
