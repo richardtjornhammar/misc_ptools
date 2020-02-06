677c1b00f3bbed061ce9e7275473a948dad3bc2c
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2020-02-06 09:17:27 +0100

    Update all_subarrays.py

--- all_subarrays.py
+++ all_subarrays.py
@@ -1,3 +1,18 @@
+"""
+Copyright 2020 RICHARD TJÖRNHAMMAR
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import pandas as pd
 import numpy as np
 import itertools
@@ -20,6 +35,9 @@ def grouper(inputs, n, fillvalue=None):
 def grouper(inputs, n):
     iters = [iter(inputs)] * n
     return zip(*iters)
+
+bucket_compare = lambda sl,sls : [ set(sl) & set(s) for s in sls if len(set(sl) & set(s))>0 ]
+length_bucket  = lambda bucket : len(bucket)
 
 if __name__ == '__main__' :
     info = """
